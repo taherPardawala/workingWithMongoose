aa53a56f1cf73ac8fd9e778a1922a82bcdb422bb
taherPardawala <skypunch2@gmail.com>
2017-06-28 01:00:33 +0530

    added a hook to the connection file for mocha

--- test/connection.js
+++ test/connection.js
@@ -1,12 +1,18 @@
 const mongoose = require('mongoose');
+
 //ES6 promise
-
 mongoose.Promise = global.Promise;
-//connect to mongodb
-mongoose.connect('mongodb://localhost/testaroo');
 
-mongoose.connection.once('open', function(){
-    console.log('connection has been made now make fireworks')
+//connect to the db before test runs
+before(function(done){
+    //connect to mongodb
+    mongoose.connect('mongodb://localhost/testaroo');
+
+    mongoose.connection.once('open', function(){
+        console.log('connection has been made now make fireworks')
+        done();
 }).on('error', function(error){
-    console.log(error);
+        console.log(error); 
+    });
 });
+
